9facdd6670ca6e79315ea8d319e122660f265f0a
Mark Cowley <mcowley@ccia.org.au>
2019-07-08 19:30:36 +1000

    refactor report logic using util.check_missing_file

--- mitylib/call.py
+++ mitylib/call.py
@@ -4,6 +4,7 @@ import subprocess
 import logging
 import os.path
 from .util import tabix
+from .util import check_missing_file
 from .normalise import do_normalise as vcfnorm
 
 # from . import normalise ## @TODO
@@ -21,10 +22,12 @@ def do_call(bam_files, reference, prefix=None, min_mq=30, min_bq=20,
     
     # Check if no prefix set that there is only one bam
     if len(bam_files) == 0:
-        raise ValueError("At least one BAM file must be supplied (-b)")
+        raise ValueError("At least one BAM file must be supplied")
     if len(bam_files) > 1 and prefix is None:
         raise ValueError(
                 "If there is more than one bam file, --prefix must be set")
+    
+    check_missing_files(bam_files, die=True)
     
     prefix = [bam_files[0].split(".")[0], prefix][prefix is not None]
     
--- mitylib/util.py
+++ mitylib/util.py
@@ -1,5 +1,6 @@
 import logging
 import subprocess
+import os
 
 def tabix(f):
     """
@@ -14,4 +15,13 @@ def tabix(f):
     tabix_call = "tabix " + f
     logging.debug(tabix_call)
     subprocess.run(tabix_call, shell=True)
+
+def check_missing_file(file_list, die=True):
+    missing_files = []
+    for item in file_list:
+        if not os.path.isfile(item):
+            missing_files.append(item)
+    if die and len(missing_files) > 0:
+        raise ValueError(f"Missing these files: {missing_files}")
+    return missing_files
 
